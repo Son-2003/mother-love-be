822617b4f59c9ff631f581647d0b442e6ceef0fc
Tran Gia Bao <baozach123@gmai.com>
2024-05-31 18:21:22 +0700

    Add project name constant

--- src/main/java/com/motherlove/utils/AppConstants.java
+++ src/main/java/com/motherlove/utils/AppConstants.java
@@ -4,6 +4,7 @@ public class AppConstants {
     private AppConstants() {
     }
 
+    public static final String PROJECT_NAME = "Mother Love";
     public static final String MESSAGE = "message";
     public static final String STATUS = "status";
     public static final String PRODUCT_NOT_FOUND = "Products not found!";
