254b3256a2d1d25f3c61ccebda7121b97c6dc8a7
Son-2003 <113695548+Son-2003@users.noreply.github.com>
2024-07-01 17:10:03 +0700

    fix(Voucher): fix query Voucher expire

--- src/main/java/com/motherlove/repositories/VoucherRepository.java
+++ src/main/java/com/motherlove/repositories/VoucherRepository.java
@@ -14,6 +14,6 @@ public interface VoucherRepository extends JpaRepository<Voucher, Long> {
     @Query("SELECT v FROM Voucher v WHERE (:now < v.endDate) and (v.quantity > 0)")
     Page<Voucher> findVouchersValidAt(LocalDateTime now, Pageable pageable);
 
-    @Query("SELECT v FROM Voucher v WHERE ((:now > v.endDate) and v.status = 1) or ((v.quantity = 0) and v.status = 1)")
+    @Query("SELECT v FROM Voucher v WHERE ((:now > v.endDate or v.quantity = 0) and v.status = 1)")
     List<Voucher> findVoucherExpire(LocalDateTime now);
 }
