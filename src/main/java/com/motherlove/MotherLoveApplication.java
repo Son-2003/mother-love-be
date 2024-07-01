984124a3f61a8293c93a4d1b98df1a6bc3cafa68
Son-2003 <113695548+Son-2003@users.noreply.github.com>
2024-07-01 17:07:11 +0700

    fix(Voucher): auto update status Expire

--- src/main/java/com/motherlove/MotherLoveApplication.java
+++ src/main/java/com/motherlove/MotherLoveApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -16,6 +17,7 @@ import java.util.List;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
+@EnableScheduling
 public class MotherLoveApplication {
 
     private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
